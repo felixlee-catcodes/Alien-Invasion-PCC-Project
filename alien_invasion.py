d78e02e6aac933cf9ee024f7a3ad459f1bc3af22
C. Felix Lee <felixlee.music@gmail.com>
2024-07-04 00:53:30 -0400

    create settings module and start on the ship module

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,14 +2,19 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
 
     def __init__(self):
         """Initialize the game, create game resources."""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
@@ -19,7 +24,8 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-
+            # Redraw the screen during each loop pass thru
+            self.screen.fill(self.settings.bg_color)
             # Make the most recently drawn screen visible.
             pygame.display.flip()
 
@@ -27,3 +33,4 @@ if __name__ == '__main__':
     # Make a game instance, run the game
     ai = AlienInvasion()
     ai.run_game()
+    
