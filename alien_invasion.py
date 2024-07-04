b99514f35fa7e1dfb839068ba8504285047543f9
C. Felix Lee <felixlee.music@gmail.com>
2024-07-04 01:13:40 -0400

    ship displaying on the bottom of the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,10 @@
+import imp
 import sys
 
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -17,6 +19,8 @@ class AlienInvasion:
         )
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
@@ -26,6 +30,7 @@ class AlienInvasion:
                     sys.exit()
             # Redraw the screen during each loop pass thru
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             # Make the most recently drawn screen visible.
             pygame.display.flip()
 
@@ -33,4 +38,3 @@ if __name__ == '__main__':
     # Make a game instance, run the game
     ai = AlienInvasion()
     ai.run_game()
-    
