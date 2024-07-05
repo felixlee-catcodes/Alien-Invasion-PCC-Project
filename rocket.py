3fb84f0907e9a7d6eeac7e73d6d7d260209ddbab
C. Felix Lee <felixlee.music@gmail.com>
2024-07-05 00:05:53 -0400

    try-it-out: space_explorer and rocket that can move in all directions complete

---
+++ rocket.py
@@ -0,0 +1,48 @@
+import pygame
+
+class Rocket:
+    """A class to manage the rocket."""
+
+    def __init__(self, se_game):
+        """Initialize the rocket and its starting position."""
+        self.screen = se_game.screen
+        self.settings = se_game.settings
+        self.screen_rect = se_game.screen.get_rect()
+
+        # Load the rocket and get its rect
+        self.image = pygame.image.load('images/rocket_ship.bmp')
+        self.resized_image = pygame.transform.scale(self.image, (60, 100))
+        self.rect = self.resized_image.get_rect()
+
+        # Sart each new ship at the bottom center of the screen.
+        self.rect.center = self.screen_rect.center
+        
+        # Store a decimal value for the ship's horizontal position.
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+        # Movement flags
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed 
+        if self.moving_up and self.rect.top > self.screen_rect.top:
+            self.y -= self.settings.ship_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed    
+        
+        #Update the rect object from self.x
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+    def blitme(self):
+        """Draw the rocket at its current location."""
+        self.screen.blit(self.resized_image, self.rect)
+        
---
+++ space_explorer.py
@@ -0,0 +1,73 @@
+import sys
+
+from click import pass_context
+import pygame
+from sqlalchemy import False_
+
+from settings import Settings
+from rocket import Rocket
+
+class SpaceExplorer:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, create game resources."""
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Space Explorer")
+
+        self.rocket = Rocket(self)
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            self._check_events()
+            self.rocket.update()
+            self._update_screen()
+        pass
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)  
+    
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.rocket.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.rocket.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.rocket.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.rocket.moving_down = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.rocket.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.rocket.moving_left = False
+        elif event.key == pygame.K_UP:
+            self.rocket.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.rocket.moving_down = False 
+
+    def _update_screen(self):
+    # Redraw the screen during each loop
+        self.screen.fill(self.settings.bg_color)
+        self.rocket.blitme()
+        # Make the most recently drawn screen visible:
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    # Make instance of the game, run the game
+    se = SpaceExplorer()
+    se.run_game()
