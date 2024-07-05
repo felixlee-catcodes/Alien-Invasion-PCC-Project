110a77778b75c60ba02645a5e476c12a6bf19327
C. Felix Lee <felixlee.music@gmail.com>
2024-07-05 13:02:02 -0400

    fixed indentation error in bullet.py; bullets fire from ship when spacebar pressed

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 from character import Character
 
 class AlienInvasion:
@@ -20,13 +21,14 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
-        # self.character = Character(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
     def _check_events(self):
@@ -46,6 +48,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()    
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()    
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
@@ -53,10 +57,17 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)      
+
     def _update_screen(self):
     # Redraw the screen during each loop pass thru
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         # Make the most recently drawn screen visible.
         pygame.display.flip()
    
