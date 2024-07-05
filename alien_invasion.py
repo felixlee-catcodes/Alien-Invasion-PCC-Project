9199fdea531ef59e2b4c46063e9482921f31e5b7
C. Felix Lee <felixlee.music@gmail.com>
2024-07-04 20:28:11 -0400

    add q key exit shortcut

--- alien_invasion.py
+++ alien_invasion.py
@@ -44,13 +44,15 @@ class AlienInvasion:
             self.ship.moving_right = True 
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()    
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
-            
+
     def _update_screen(self):
     # Redraw the screen during each loop pass thru
         self.screen.fill(self.settings.bg_color)
