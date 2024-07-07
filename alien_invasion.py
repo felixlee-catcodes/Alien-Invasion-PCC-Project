f5fb758c5c3fad5b802e699e1bfa9b33a5ebc0da
C. Felix Lee <felixlee.music@gmail.com>
2024-07-06 23:52:33 -0400

    add alien image bmp to images folder

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,8 +5,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
-from character import Character
-
+# from character import Character
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
 
@@ -28,8 +27,8 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-            self._update_screen()
+            self._update_bullets()
+            self._update_screen()        
 
     def _check_events(self):
         """Respond to keypresses and mouse events."""
@@ -59,8 +58,20 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)      
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)      
+
+    def _update_bullets(self):
+        """Update the position of bullets and remove old bullets."""
+        # Update bullet positions:
+        self.bullets.update()
+
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
 
     def _update_screen(self):
     # Redraw the screen during each loop pass thru
