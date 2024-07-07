b3dee17b948ff9233ddd14fda6fe9615acd16363
C. Felix Lee <felixlee.music@gmail.com>
2024-07-07 00:18:11 -0400

    1st alien drawn on screen

--- alien.py
+++ alien.py
@@ -1,7 +1,7 @@
 import pygame
 from pygame.sprite import Sprite
 
-class Alien:
+class Alien(Sprite):
     """A class to represent a single alien in the fleet."""
     def __init__(self, ai_game):
        """Initialize the alien and its starting position."""
@@ -17,4 +17,4 @@ class Alien:
        self.rect.y = self.rect.height
 
        # Store the alien's exact horizontal position:
-       self.x = float(self.x)
+       self.x = float(self.rect.x)
