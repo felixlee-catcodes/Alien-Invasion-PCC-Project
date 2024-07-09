d4ac20756c86b487ff196572b032e1bb44c01ebd
C. Felix Lee <felixlee.music@gmail.com>
2024-07-09 16:28:28 -0400

    create Scoreboard class

--- game_stats.py
+++ game_stats.py
@@ -5,10 +5,11 @@ class GameStats:
         """Initialize statistics."""
         self.settings = ai_game.settings
         self.reset_stats()
-        
+
         # Start game in an inactive state:
         self.game_active = False
 
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
-        self.ships_left = self.settings.ship_limit    
+        self.ships_left = self.settings.ship_limit  
+        self.score = 0  
