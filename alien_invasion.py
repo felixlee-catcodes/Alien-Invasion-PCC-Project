ded5c43634e8944c66a10ca69519235f199ab390
C. Felix Lee <felixlee.music@gmail.com>
2024-07-09 18:03:58 -0400

    refined collision detection to catch every alien hit using the collisions dictionary

--- alien_invasion.py
+++ alien_invasion.py
@@ -73,6 +73,7 @@ class AlienInvasion:
             # Reset the game statistics:
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
 
             # Get rid of any remaining aliens and bullets:
             self.aliens.empty()
@@ -126,9 +127,10 @@ class AlienInvasion:
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)  
 
         if collisions: 
-            self.stats.score += self.settings.alien_points
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
-            
+
         if not self.aliens:
             # Destroy existing bullets and create a new fleet:
             self.bullets.empty()
