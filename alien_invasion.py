12df31a799173216ef196583d9fc7f8241a041c3
C. Felix Lee <felixlee.music@gmail.com>
2024-07-09 19:23:04 -0400

    ships/lives left displayed in upper left corner, update as # changes; alien invasion main functionality complete

--- alien_invasion.py
+++ alien_invasion.py
@@ -73,7 +73,9 @@ class AlienInvasion:
             # Reset the game statistics:
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_level()
             self.sb.prep_score()
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets:
             self.aliens.empty()
@@ -159,6 +161,7 @@ class AlienInvasion:
         if self.stats.ships_left > 0:
             # Decrement ships left:
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets:
             self.aliens.empty()
